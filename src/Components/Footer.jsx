7e19c2896eea61e05ae7063708976a05c7b7d9fd
Ivan Tarasiuk <viix345@gmail.com>
2023-03-07 03:14:54 +0300

    modified something

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
           © {new Date().getFullYear()} Copyright Text
           <a
             className="grey-text text-lighten-4 right"
-            href="https://github.com/viix54/reactShop3"
+            href="https://github.com/viix54/reactShop4"
             target="_blank"
             rel="noreferrer"
           >
